780c5715062c9484d520226e7671b6a736ad26f9
Jason Connell <jasontconnell@gmail.com>
2014-12-26 16:37:20 -0500

    Site is now 95% complete

--- common.go
+++ common.go
@@ -6,8 +6,12 @@ import (
 )
 
 func MakeKey(str string) string {
-	reg := regexp.MustCompile(`[^a-zA-Z0-9\-\\]+`)
-	str = reg.ReplaceAllString(str, "-")
+	reg := regexp.MustCompile(`[^a-zA-Z0-9\-\\ ]+`)
+	spacereg := regexp.MustCompile(`[ ]+`)
+	str = reg.ReplaceAllString(str, "")
+	str = strings.Trim(str, " ")
+	str = spacereg.ReplaceAllString(str, " ")
+	str = spacereg.ReplaceAllString(str, "-")
 	str = strings.ToLower(str)
 	return str
 }
